c2e6312e02b802ac4b7063e4089e1c7ccaa06b85
IslamAhmedKamel <https://github.com/IslamAhmedKamel>
2026-03-24 04:18:31 +0200

    create a MyCustom Button and convet statelesswidget to statefulwidget and  create a rest button after that finsfed app

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,8 +1,17 @@
+import 'package:first_project/Components/myCustomButton.dart'
+    show MyCustomButton;
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int countA = 0;
+  int countB = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,27 +29,99 @@ class MyHomePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text("Team A", style: TextStyle(fontSize: 25)),
-                    Text(
-                      "0",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    Column(
+                      children: [
+                        Text("Team A", style: TextStyle(fontSize: 25)),
+                        Text(
+                          "$countA",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        MyCustomButton(
+                          string: 'Add 1 Point ',
+                          onPressed: () {
+                            setState(() {
+                              countA++;
+                            });
+                          },
+                        ),
+
+                        MyCustomButton(
+                          string: 'Add 2 Point ',
+                          onPressed: () {
+                            setState(() {
+                              countA += 2;
+                            });
+                          },
+                        ),
+
+                        MyCustomButton(
+                          string: 'Add 3 Point ',
+                          onPressed: () {
+                            setState(() {
+                              countA += 3;
+                            });
+                          },
+                        ),
+                      ],
                     ),
                   ],
                 ),
+
                 VerticalDivider(thickness: 2, color: Colors.orange),
+
                 Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    Text("Team B", style: TextStyle(fontSize: 25)),
-                    Text(
-                      "0",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
+                    Column(
+                      children: [
+                        Text("Team B", style: TextStyle(fontSize: 25)),
+                        Text(
+                          "$countB",
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Column(
+                      children: [
+                        MyCustomButton(
+                          string: 'Add 1 Point ',
+                          onPressed: () {
+                            setState(() {
+                              countB++;
+                            });
+                          },
+                        ),
+                        MyCustomButton(
+                          string: 'Add 2 Points ',
+                          onPressed: () {
+                            setState(() {
+                              countB += 2;
+                            });
+                          },
+                        ),
+                        MyCustomButton(
+                          string: 'Add 3 Points ',
+                          onPressed: () {
+                            setState(() {
+                              countB += 3;
+                            });
+                          },
+                        ),
+                      ],
                     ),
                   ],
                 ),
@@ -49,16 +130,16 @@ class MyHomePage extends StatelessWidget {
           ),
           Spacer(flex: 1),
 
-          ElevatedButton(
-            onPressed: () {},
-            child: Text("Rest Counter", style: TextStyle(color: Colors.white)),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.orange,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-            ),
+          MyCustomButton(
+            string: "Rest Counter9",
+            onPressed: () {
+              setState(() {
+                countA = 0;
+                countB = 0;
+              });
+            },
           ),
+
           Spacer(flex: 3),
         ],
       ),
